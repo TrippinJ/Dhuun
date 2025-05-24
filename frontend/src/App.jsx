e1370f31ee24c8627541ebcef8e338fec6b35372
JohnsonBantawa <NP03CS4A220288@heraldcollege.edu.np>
2025-05-24 23:52:14 +0545

    Bug Fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Home from "./Components/Home";
 import About from "./Components/About";
 import Work from "./Components/Work";
 import Testimonial from "./Components/Testimonial";
-import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Login from "./pages/Login"
@@ -67,7 +66,6 @@ function App() {
                             <About />
                             <Work />
                             <Testimonial />
-                            <Contact />
                             <Footer />
                           </>
                         }
