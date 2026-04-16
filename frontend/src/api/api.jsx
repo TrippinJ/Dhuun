79b508a7c74321841f62a2a23d925e49aff224b3
JohnsonBantawa <NP03CS4A220288@heraldcollege.edu.np>
2026-04-17 01:59:36 +0545

    fix:new update

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:8080",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "https://dhuun-backend.onrender.com",
   // headers: {
   //   "Content-Type": "application/json",
   // },
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://dhuun-backend.onrender.com',
         changeOrigin: true,
       }
     }
